46d12eb350a96cd84c4d8b3d11ff4c8a692bf349
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-04-04 15:34:03 -0300

    feat: implement the Config method

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // Route represents all of the API routes
 type Route struct {
@@ -8,4 +12,15 @@ type Route struct {
 	Method      string
 	Function    func(http.ResponseWriter, *http.Request)
 	RequireAuth bool
+}
+
+//Config put all the routes into the router
+func Config(r *mux.Router) *mux.Router {
+	routes := usersRoutes
+
+	for _, route := range routes {
+		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
+	}
+
+	return r
 }
