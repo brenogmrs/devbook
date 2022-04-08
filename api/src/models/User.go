1bdfe12d089fda9b8963b5e6fbbb8d48645dbf32
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-04-08 13:17:08 -0300

    feat: add and implement the field validation (improve code)

--- api/src/models/User.go
+++ api/src/models/User.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // User model
 type User struct {
@@ -11,4 +15,36 @@ type User struct {
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"created_at,omitempty"`
 	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
+
+func (user *User) Prepare() error {
+	if err := user.validateFields(); err != nil {
+		return err
+	}
+
+	user.formatData()
+	return nil
+}
+
+func (user *User) validateFields() error {
+	if user.Name == "" {
+		return errors.New("field `name` is required")
+	}
+	if user.Email == "" {
+		return errors.New("field `email` is required")
+	}
+	if user.Nick == "" {
+		return errors.New("field `nick` is required")
+	}
+	if user.Password == "" {
+		return errors.New("field `password` is required")
+	}
+
+	return nil
+}
+
+func (user *User) formatData() {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
