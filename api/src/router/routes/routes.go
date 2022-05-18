43c0788685fe5fb3c64e132a8d9fb60c1855df7e
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-18 12:14:30 -0300

    feat: add login route

--- api/src/router/routes/routes.go
+++ api/src/router/routes/routes.go
@@ -17,6 +17,7 @@ type Route struct {
 //Config put all the routes into the router
 func Config(r *mux.Router) *mux.Router {
 	routes := usersRoutes
+	routes = append(routes, loginRoute)
 
 	for _, route := range routes {
 		r.HandleFunc(route.URI, route.Function).Methods(route.Method)
