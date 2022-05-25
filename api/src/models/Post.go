1955244d6e53438e4675d868ff714c918b0c83c0
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-25 14:26:13 -0300

    feat: add post struct

---
+++ api/src/models/Post.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type Post struct {
+	ID          uint64    `json:"id,omitempty"`
+	Title       string    `json:"title,omitempty"`
+	Content     string    `json:"content,omitempty"`
+	AuthourID   uint64    `json:"author_id,omitempty"`
+	AuthourNick string    `json:"author_nick,omitempty"`
+	Likes       uint64    `json:"likes"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+}
