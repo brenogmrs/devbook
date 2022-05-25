e96716b498b2f467fc43615a214e1344f4df7b1c
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-25 14:41:11 -0300

    feat: add posts script

--- api/sql/util.sql
+++ api/sql/util.sql
@@ -2,6 +2,8 @@ CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS posts;
 
 CREATE TABLE users(
     id int auto_increment primary key,
@@ -29,4 +31,19 @@ CREATE TABLE followers(
     updated_at timestamp default current_timestamp(),
 
     primary key(user_id, follower_id)
+) ENGINE=INNODB;
+
+CREATE TABLE posts(
+    id int auto_increment primary key,
+    title varchar(50) not null,
+    content varchar(300) not null,
+
+    author_id int not null,
+    FOREIGN KEY (author_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,
+
+    likes int default 0,
+    created_at timestamp default current_timestamp(),
+    updated_at timestamp default current_timestamp()
 ) ENGINE=INNODB;
