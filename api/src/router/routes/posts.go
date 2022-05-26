4db228060d5c9c80e375d1e75bc8781a321f15af
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-26 10:11:07 -0300

    feat: add routes and controller methods

---
+++ api/src/router/routes/posts.go
@@ -0,0 +1,39 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var postsRoutes = []Route{
+	{
+		URI:         "/post",
+		Method:      http.MethodPost,
+		Function:    controllers.CreatePost,
+		RequireAuth: true,
+	},
+	{
+		URI:         "/post/{postID}",
+		Method:      http.MethodGet,
+		Function:    controllers.GetAllPosts,
+		RequireAuth: true,
+	},
+	{
+		URI:         "/post/{postID}",
+		Method:      http.MethodGet,
+		Function:    controllers.GetPostById,
+		RequireAuth: true,
+	},
+	{
+		URI:         "/post/{postID}",
+		Method:      http.MethodPut,
+		Function:    controllers.UpdatePost,
+		RequireAuth: true,
+	},
+	{
+		URI:         "/post/{postID}",
+		Method:      http.MethodDelete,
+		Function:    controllers.DeletePost,
+		RequireAuth: true,
+	},
+}
