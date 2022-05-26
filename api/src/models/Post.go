a1c909328961e4a3016c5f93b2f779f59b28338c
Breno Guimaraes <brenoaraujo@outlook.com.br>
2022-05-26 11:52:12 -0300

    feat: add fields validation

--- api/src/models/Post.go
+++ api/src/models/Post.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Post struct {
 	ID          uint64    `json:"id,omitempty"`
@@ -11,4 +15,30 @@ type Post struct {
 	Likes       uint64    `json:"likes"`
 	CreatedAt   time.Time `json:"created_at,omitempty"`
 	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+}
+
+func (post *Post) Prepare() error {
+	if err := post.validateFields(); err != nil {
+		return err
+	}
+
+	post.formatFields()
+	return nil
+}
+
+func (post *Post) validateFields() error {
+	if post.Title == "" {
+		return errors.New("field `title` is required")
+	}
+
+	if post.Content == "" {
+		return errors.New("field `content` is required")
+	}
+
+	return nil
+}
+
+func (post *Post) formatFields() {
+	post.Title = strings.TrimSpace(post.Title)
+	post.Content = strings.TrimSpace(post.Content)
 }
